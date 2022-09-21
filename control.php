a0aa8d7e0db0f4340df4bf0b9dd8c094143b22d3
arabicps4games <80811378+arabicps4games@users.noreply.github.com>
2022-09-21 04:02:54 +0100

    test
    test

---
+++ control.php
@@ -0,0 +1,22 @@
+
+<?php
+	session_start();
+	if (isset($_SESSION['user'])) {
+		echo 'Welcome ' . $_SESSION['user'] . ' You Are Admin';
+		echo '<pre>;
+		print_r($_SESSION);
+		echo '<pre>;
+	} else {
+			echo 'You Are Not Permitted To See This Page';
+		}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ log.php
@@ -0,0 +1,5 @@
+<?php
+<form action="chk.php" method="POST">
+	<input type="text" name="username">
+	<input type="submit" value="Login">
+</from>
